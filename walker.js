6035dcd81f4813e7ce730f5f50a8d3bf3f45958f
epsalt <evan.saltman@gmail.com>
2016-11-29 03:02:55 -0700

    Javascript cleanup various improvemnts
    - Variable scoping cleaned up considerably
    - Better spacing
    - Single quotes changed to double
    - More concise

--- walker.js
+++ walker.js
@@ -1,34 +1,29 @@
-function draw(){
+function draw() {
     // Random walk canvas animation
-    var canvas = document.getElementById('walker');
-    if (canvas.getContext){
-        var ctx = canvas.getContext('2d');
-
-        var nrows = 75;
-        var ncols = Math.round(canvas.scrollHeight / canvas.scrollWidth * nrows);
-        
-        var cell_width = canvas.width / nrows;
-        var cell_height = canvas.height / ncols;
-
+    var canvas = document.getElementById("walker");
+    if (canvas.getContext) {
+        var ctx = canvas.getContext("2d");
+        var nrows = 120;
+        var dims = {"nrows": nrows,
+                    "ncols": Math.round(canvas.height / canvas.width * nrows),
+                    "width": canvas.width / nrows,
+                    "height": canvas.height / Math.round(canvas.height / canvas.width * nrows)};
         ctx.fillStyle = "rgba(0, 0, 200, 0.25)";
+        var coords = [0, Math.round(dims.ncols / 2)]; // start on the middle of the left side, walk right
+        animate(coords, ctx, dims);
+    }
+}
 
-        // start on the middle of the left side, walk right
-        var coords = [0, (ncols/2)];
-
-        function animate(coords){
-            // if the walker goes out of bounds reset
-            if(coords[0] > nrows ||
-               coords[1] < 1     ||
-               coords[1] > ncols - 2) {
-                setTimeout(animate, 750, [0, ncols/2]);
-            } else {
-                // take a step and draw a rect
-                var step = Math.round((Math.random() * 2) - 1);
-                coords = [coords[0] + 1, coords[1] + step];          
-                ctx.fillRect(coords[0]*cell_width, coords[1]*cell_height, cell_width, cell_height);
-                setTimeout(animate, 200, coords);
-            }
-        }
-        animate(coords);
+function animate(coords, ctx, dims) {
+    // if the walker goes out of bounds reset
+    if (coords[0] >= dims.nrows ||
+        coords[1] <= 0 ||
+        coords[1] >= dims.ncols) {
+        setTimeout(animate, 150, [0, Math.round(dims.ncols / 2)], ctx, dims);
+    } else { // draw a rect then take a step        
+        ctx.fillRect(coords[0] * dims.width, coords[1] * dims.height, dims.width, dims.height);        
+        var step = Math.round((Math.random() * 2) - 1);
+        coords = [coords[0] + 1, coords[1] + step];
+        setTimeout(animate, 150, coords, ctx, dims);
     }
 }
