f2528bfc2489e142c8db378ae8f41c25643c69eb
epsalt <evan.saltman@gmail.com>
2017-02-02 15:14:02 -0700

    Switch from setTimeout to requestAnimationFrame

--- walker.js
+++ walker.js
@@ -1,3 +1,8 @@
+(function () {
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
+})();
+
 function draw() {
     // Random walk canvas animation
     var canvas = document.getElementById("walker");
@@ -19,12 +24,16 @@ function animate(coords, ctx, dims) {
     if (coords[0] >= dims.nrows ||
         coords[1] <= 0 ||
         coords[1] >= dims.ncols) {
-        setTimeout(animate, 150, [0, Math.round(dims.ncols / 2)], ctx, dims);
+        requestAnimationFrame(function(){
+            animate([0, Math.round(dims.ncols / 2)], ctx, dims);
+        });
     } else {
         // draw a rect then take a step        
         ctx.fillRect(coords[0] * dims.width, coords[1] * dims.height, dims.width, dims.height);        
         var step = Math.round((Math.random() * 2) - 1);
         coords = [coords[0] + 1, coords[1] + step];
-        setTimeout(animate, 150, coords, ctx, dims);
+        requestAnimationFrame(function(){
+            animate(coords, ctx, dims);
+        });
     }
 }
