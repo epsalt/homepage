8b46f7e29b5f950aaa68a84dbb393fc02a375e5d
epsalt <evan.saltman@gmail.com>
2018-03-06 21:40:14 -0700

    refactor: use more classes in site_generator

--- site_generator.py
+++ site_generator.py
@@ -14,58 +14,145 @@ from feedgen.feed import FeedGenerator
 from markdown import Markdown
 from jinja2 import FileSystemLoader, Environment
 
-POST_DIR = 'posts/'
-TEMPLATE_DIR = 'templates'
-PROJ_DIR = 'projects'
-SITE_DIR = 'site/'
-SITE_URL = 'https://epsalt.ca/'
-IMAGE_DIR = 'images/'
+dirs = {
+    'posts': 'content/posts/',
+    'projects': 'content/projects',
+    'root': 'content/root',
+    'tag': 'content/tag',
+    'templates': 'templates',
+    'site': 'site/',
+    'images': 'images/'
+}
 
+site = {
+    'url': 'https://epsalt.ca/',
+    'title': 'from the desk of e.p salt',
+    'name': 'E.P Salt',
+    'email': 'evan@epsalt.ca'
+}
+
+class SiteGenerator(object):
+    def __init__(self, dirs, site):
+        self.dirs = dirs
+        self.site = site
+
+    def publish(self):
+        self.render_posts()
+        self.render_tags()
+        self.render_root()
+        self.render_projects()
+        self.render_rss()
+
+    def render_posts(self):
+        self.posts = []
+        for post in listdir(self.dirs['posts']):
+            self.posts.append(Post(self.dirs['posts'], post, self.dirs, self.site))
+        
+        self.posts = sorted(self.posts, key=lambda x: x.date, reverse=True)
+
+        # Populate previous and next post attributes
+        for post in self.posts:
+            post.set_neighbours(self.posts)
+
+        self.args = {'posts': self.posts}
+
+        for post in self.posts:
+            post.render('post.html', self.args, post.link)
+
+        ## Render index
+        index_args = self.args
+        index_args['index'] = True
+
+        index_post = self.posts[0]
+        index_post.render('post.html', index_args, 'index.html')
+        
+    def render_tags(self):
+        makedirs(join(self.dirs['site'], "tag"))
+
+        tag_dict = defaultdict(list)
+        for post in self.posts:
+            for tag in post.meta.get('tags'):
+                tag_dict[tag].append(post)
+
+        for tag, tagged_posts in tag_dict.items():
+            page = Page(self.dirs['root'], 'tag.md', self.dirs)
+            args = {'posts': self.posts,
+                    'tag_posts': tagged_posts,
+                    'tag': tag}
+            out = join("tag", tag)
+
+            page.render('tag.html', args, out)
+
+    def render_root(self):
+        for root_page in listdir(self.dirs['root']):
+            page = Page(self.dirs['root'], root_page, self.dirs)
+            url = page.meta.get('url')
+            if url:   
+                page.render(url + '.html', self.args, url)
+
+    def render_projects(self):
+        for project in listdir(self.dirs['projects']):
+            page = Page(self.dirs['projects'], project, self.dirs)
+            out = join(self.dirs['projects'], page.meta.get('url'))
+            page.render("project.html", self.args, out)
+
+    def render_rss(self):
+        feed = FeedGenerator()
+        feed.id(self.site['url'])
+        feed.title(self.site['title'])
+        feed.author({'name': self.site['name'], 'email': self.site['email']})
+        feed.link(href=self.site['url'], rel="self")
+        feed.language('en')
+
+        for post in self.posts:
+            post.set_rss_attributes(feed)
+
+        feed.atom_file(join(self.dirs['site'], 'rss'), pretty=True)
+                    
 class Page(object):
     """Base class for static website page rendering"""
 
-    def __init__(self, directory, fname):
+    def __init__(self, directory, fname, dirs):
+        self.dirs = dirs
+        self.path = join(directory, fname)
+        self.read_markdown()
+
+    def read_markdown(self):
         md = Markdown(extensions=['markdown.extensions.extra',
                                   'markdown.extensions.meta',
                                   'markdown.extensions.smarty'],
                       output_format='html5')
 
-        path = join(directory, fname)
-
-        with open(md_fname, 'r') as path:
-            self.html = md.convert(path.read())
-
-        # Remove list wrapping from everything except actual lists
+        with open(self.path, 'r') as f:
+            self.html = md.convert(f.read())
+            
         self.meta = {key: value if key == "tags" else value[0]
                      for key, value in md.Meta.items()}
 
-    def render(self, template, template_args, outpath=None):
+    def render(self, template, args, outpath=None):
         """Method for rendering Pages with markdown and jinja2"""
 
-        outpath = join(SITE_DIR, outpath)
+        outpath = join(self.dirs['site'], outpath)
 
-        # Build jinja template argument dict
-        args = {'post': self,
-                'content': self.html}
-        for key, value in template_args.items():
-            args[key] = value
+        args['post'] = self
+        args['content'] = self.html
 
         # Create directory if necessary
         if not exists(dirname(outpath)):
             makedirs(dirname(outpath))
 
         # Use jinja to render template and save
-        env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+        loader = FileSystemLoader(self.dirs['templates'])
+        env = Environment(loader=loader)
         text = env.get_template(template).render(args)
         with open(outpath, 'w') as outfile:
             outfile.write(text)
 
 class Post(Page):
-    """Class for blog posts, extends Page"""
+    def __init__(self, directory, fname, dirs, site):
+        Page.__init__(self, directory, fname, dirs)
 
-    def __init__(self, directory, fname):
-        Page.__init__(self, directory, fname)
-
+        self.site = site
         self.date = parse(self.meta.get('date'))
         self.directory = join(self.date.strftime('%Y'),
                               self.date.strftime('%m'))
@@ -75,7 +162,7 @@ class Post(Page):
 
     def set_neighbours(self, posts):
         """
-        Sets npost and ppost attributes.
+        Sets next and previous post attributes.
 
         Given a list of Posts, this method sets the npost and ppost
         attributes. It is necessary that the post using this method
@@ -98,106 +185,24 @@ class Post(Page):
 
     def set_rss_attributes(self, feed):
         """
-        Update RSS atrributes.
+        Set RSS atrributes for a post.
 
         Given a python-feedgenerator object, this adds an entry for
         the post from the post content and metadata.
 
         """
 
-        # Metadata tags
         feed_entry = feed.add_entry()
-        feed_entry.id(join(SITE_URL, self.link))
+        feed_entry.id(join(self.site['url'], self.link))
         feed_entry.title(self.meta.get('title'))
         feed_entry.published(self.date)
         feed_entry.updated(self.meta.get('updated'))
 
         # Full content with hardlinked images
-        content = self.html.replace('src="/'+IMAGE_DIR,
-                                    'src="{}'.format(join(SITE_URL, IMAGE_DIR)))
+        content = self.html.replace('src="/' + self.dirs['images'],
+                                    'src="{}'.format(join(self.site['url'], self.dirs['images'])))
         feed_entry.content(content, type="html")
-
-def build_post_list():
-    # Get blog posts from POST_DIR
-    posts = [Post(POST_DIR, post) for post in listdir(POST_DIR)]
-    sorted_posts = sorted(posts, key=lambda x: x.date, reverse=True)
-
-    # Add previous and next post attributes to Post Class
-    for post in sorted_posts:
-        post.set_neighbours(sorted_posts)
-
-    return sorted_posts
-
-def render_tag_pages(posts):
-    """Render tag pages from list of posts"""
-
-    # Build tag dict
-    makedirs(join(SITE_DIR, "tag"))
-    tag_dict = defaultdict(list)
-    for post in posts:
-        for tag in post.meta.get('tags'):
-            tag_dict[tag].append(post)
-
-    # Render tag pages
-    for tag, tag_posts in tag_dict.items():
-        tag_args = {'posts': posts,
-                    'tag_posts': tag_posts,
-                    'tag': tag}
-        Page(TEMPLATE_DIR, 'tag.md').render('tag.html', tag_args, join("tag", tag))
-
-def build_rss_feed(posts):
-    """Create RSS feed from list of posts"""
-
-    feed = FeedGenerator()
-    feed.id(SITE_URL)
-    feed.title('from the desk of e.p salt')
-    feed.author({'name':'E.P Salt', 'email':'evan@epsalt.ca'})
-    feed.link(href=SITE_URL, rel="self")
-    feed.language('en')
-
-    for post in posts:
-        post.set_rss_attributes(feed)
-
-    # Render RSS page
-    feed.atom_file(join(SITE_DIR, 'rss'), pretty=True)
-
-def publish():
-    """
-    Generate all static site pages.
-
-    This function finds all of the following and renders them as
-    html in the SITE_DIR:
-    - The about.md and archive.md file in the root directory
-    - All posts in POST_DIR
-    - All post tag files
-    """
-
-    posts = build_post_list()
-
-    # Jinja2 template arguments
-    args = {'posts': posts}
-
-    # Render all blog posts
-    for post in posts:
-        post.render('post.html', args, post.link)
-
-    # Render site root pages
-    for root in ['about', 'archive', '404']:
-        root_page = Page(TEMPLATE_DIR, root + '.md')
-        root_page.render(root + '.html', args, root_page.meta.get('url'))
-
-    # Render project pages
-    projects = [Page(PROJ_DIR, proj) for proj in listdir(PROJ_DIR)]
-    for project in projects:
-        project.render("project.html", args, join(PROJ_DIR, project.meta.get('url')))
-
-    # Index page
-    index_args = args
-    index_args['index'] = True
-    posts[0].render('post.html', index_args, 'index.html')
-
-    render_tag_pages(posts)
-    build_rss_feed(posts)
-
+    
 if __name__ == "__main__":
-    publish()
+    generator = SiteGenerator(dirs, site)
+    generator.publish()
