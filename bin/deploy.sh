c5f22cd2806741f2f66d67024cea63642083b887
Evan Saltman <evan.saltman@gmail.com>
2021-01-01 15:58:23 -0700

    updated deployment

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-aws s3 sync site s3://$BUCKET \
+aws s3 sync site s3://${BUCKET} \
     --size-only \
     --delete \
     --exclude "*" \
     --include "rss" \
-    --cache-control max-age=$LONG_CACHE \
+    --cache-control max-age=${LONG_CACHE} \
     --content-type 'application/xml'
 
-aws s3 sync site s3://$BUCKET/ \
+aws s3 sync site s3://${BUCKET}/ \
     --size-only \
     --delete \
     --exclude "*.*" \
     --include "*.html" \
-    --cache-control max-age=$SHORT_CACHE \
+    --cache-control max-age=${SHORT_CACHE} \
     --content-type 'text/html'
 
-aws s3 sync site s3://$BUCKET \
+aws s3 sync site s3://${BUCKET} \
     --size-only \
     --delete \
-    --cache-control max-age=$LONG_CACHE
+    --cache-control max-age=${LONG_CACHE}
 
-aws s3 cp s3://$CHALLENGE_BUCKET s3://$BUCKET/ \
-    --recursive
+if [[ -z "${CHALLENGE_BUCKET}" ]]; then
+    aws s3 cp s3://${CHALLENGE_BUCKET} s3://${BUCKET}/ \
+	--recursive
+fi
 
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths '/*'
+if [[ -z "${CLOUDFRONT_ID}" ]]; then
+    aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_ID} --paths '/*'
+fi
